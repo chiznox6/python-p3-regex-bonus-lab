bfecfaf0409f8ee872e60410738fa7e24f1567c2
James <james.kariuki5@student.moringaschool.com>
2025-08-29 21:05:43 +0300

    initial commit

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,8 @@
 import re
 
-my_pattern = r""
+# Match only the three sentences exactly
+my_pattern = r"(It's such a lovely day today\.|Some weather we're having today, huh\?|Maybe today's just not my day\.)"
+
+# Compile the regex
 my_regex = re.compile(my_pattern)
 
